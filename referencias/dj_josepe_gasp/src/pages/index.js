b8f21cabb929f9b7edf370dd668de4bb2665827b
kikkobcn <kikobonometti@gmail.com>
2025-09-06 17:03:46 +0200

    Add referencias folder with GSAP video parallax reference implementation

---
+++ referencias/dj_josepe_gasp/src/pages/index.js
@@ -0,0 +1,13 @@
+import Video from "@/components/Video"
+
+function Home() {
+  return (
+    <>
+      <div className="header"></div>
+      <Video />
+      <div className="footer"></div>
+    </>
+  )
+}
+
+export default Home
